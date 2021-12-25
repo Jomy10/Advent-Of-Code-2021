aa9ee775499244ebd81a4dca4768e64741df72c2
Jomy10 <jonas.vbs4@gmail.com>
2021-12-25 23:40:52 +0100

    Changed type of input in day 8

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
 //! day 8
 
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let input = fs::read_to_string("src/inputs/day8.txt").unwrap();
-    let input: Vec<Vec<&str>> = input.lines().map(|line| {
+    let input: Vec<(&str, &str)> = input.lines().map(|line| {
         let vec = line.split("|").collect::<Vec<&str>>();
-        vec.into_iter().map(|val| val.trim()).collect::<Vec<&str>>()
+        let vec = vec.into_iter().map(|val| val.trim()).collect::<Vec<&str>>();
+        (vec[0], vec[1])
     }).collect();
+    
+    let transform: HashMap<char, char> = HashMap::from(
+        [
+            ('a','d'),
+            ('b','e'),
+            ('c','a'),
+            ('d','f'),
+            ('e','g'),
+            ('f','b'),
+            ('g','c')
+        ]
+    );
+    
+    let patterns: HashMap<u8, &str> = HashMap::from(
+      [
+          (0, "abcefg"),
+          (1, "cf"),
+          (2, "acdeg"),
+          (3, "acdfg"),
+          (4, "bcdf"),
+          (5, "abcfg"),
+          (6, "abdefg"),
+          (7, "acf"),
+          (8, "abcdefg"),
+          (9, "abcdfg")
+      ]
+    );
+    
+    println!("{:?}", transform);
     
     // 1, 4, 7, 8
     let mut value_amts = [0; 4];
     
+    let mut sum: usize = 0;
     input.into_iter().for_each(|val| {
-        let digits: Vec<&str> = val[1].split(" ").collect();
+        let digits: Vec<&str> = val.1.split(" ").collect();
         for digit in digits {
             match digit.len() {
                 2 => value_amts[0] += 1,
